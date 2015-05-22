48ffa6df089df2fd5c9dca669d5ad093eed66a26
Nicolas Hubert <nicolas.hubert.1@ens.etsmtl.ca>
2015-05-22 16:55:19 -0400

    Dernière modif avant javadoc

--- src/controller/ConversionHSVversRGB.java
+++ src/controller/ConversionHSVversRGB.java
@@ -1,85 +1,4 @@
-<<<<<<< HEAD
-package controller;
-/**
- * COURS_GTI_410 : 
- * LABORATOIRE_1
- * 
- * EQUIPE : 
- * 			Idriss Aissou AISI01088901
- * 			Nicolas Hubert
- */
 
-/**
- * Classe qui permet de convertir du format HSV au RGB
- *
- */
-public class ConversionHSVversRGB {
-
-	/**
-	 * Constructeur par defaut de la classe ConversionHSVversRGB
-	 */
-	public ConversionHSVversRGB(){}
-	
-	/**
-	 * Methode permettant de convertir le hsv eb rgb
-	 * @param hRecu (teinte)
-	 * @param sRecu (saturation)
-	 * @param vRecu (valeur)
-	 * @return int []Tableau RGB
-	 */
-	public int[] HSVversRGB (double hRecu, double sRecu, double vRecu){
-		int[] RGB_TAB =  new int[3];
-		double[] RGB_tmp =  new double[3];
-		double c;
-		double x;
-		double m;
-		c= (vRecu * sRecu);
-		x = c * (1-Math.abs((hRecu/60 % 2) -1));
-		m= (vRecu - c);
-		 //System.out.println("CONVERSION EN RGB (c,x,m) : "+c+" "+x+" "+m);
-       // System.out.println("CONVERSION EN RGB (HSV RECU) : "+hRecu+" "+sRecu+" "+vRecu);
-
-		if(hRecu>=0 & hRecu<60){
-			RGB_tmp[0]= c;
-			RGB_tmp[1]= x;
-			RGB_tmp[2]= 0;
-		}
-		else if(hRecu>=60 & hRecu<120){
-			RGB_tmp[0]= x;
-			RGB_tmp[1]= c;
-			RGB_tmp[2]= 0;
-		}
-		else if(hRecu>=120 & hRecu<180){
-			RGB_tmp[0]= 0;
-			RGB_tmp[1]= c;
-			RGB_tmp[2]= x;
-		}
-		else if(hRecu>=180 & hRecu<240){
-			RGB_tmp[0]= 0;
-			RGB_tmp[1]= x;
-			RGB_tmp[2]= c;
-		}
-		else if(hRecu>=240 & hRecu<300){
-			RGB_tmp[0]= x;
-			RGB_tmp[1]= 0;
-			RGB_tmp[2]= c;
-		}
-		else if(hRecu>=300 & hRecu<360){
-			RGB_tmp[0]= c;
-			RGB_tmp[1]= 0;
-			RGB_tmp[2]= x;
-		}
-		RGB_TAB[0]= (int) Math.round((RGB_tmp[0]+m)*255);
-		RGB_TAB[1]= (int) Math.round((RGB_tmp[1]+m)*255);
-		RGB_TAB[2]= (int) Math.round((RGB_tmp[2]+m)*255);
-        
-        //System.out.println("CONVERSION EN RGB : "+RGB_TAB[0]+" "+RGB_TAB[1]+" "+RGB_TAB[2]);
-        
-		return RGB_TAB;
-	}
-	
-}
-=======
 package controller;
 /**
  * COURS_GTI_410 : 
@@ -162,4 +81,4 @@ public class ConversionHSVversRGB {
 	}
 	
 }
->>>>>>> branch 'master' of https://github.com/nicolashub/GTI410_LAB1.git
+
