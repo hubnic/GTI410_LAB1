d84466add4d41b6fa99b1ebcb6a8054b8ca84f5b
Aissou Idriss <i7.aissou@gmail.com>
2015-06-25 14:09:51 -0400

    Correction Clamp

--- src/controller/ImageClampAbsNormalizeTo255.java
+++ src/controller/ImageClampAbsNormalizeTo255.java
@@ -7,38 +7,85 @@ import model.PixelDouble;
 
 public class ImageClampAbsNormalizeTo255 extends ImageClampStrategy {
 
+	double[] minMax;
 	public ImageX convert(ImageDouble image) {
 		int imageWidth = image.getImageWidth();
 		int imageHeight = image.getImageHeight();
 		ImageX newImage = new ImageX(0, 0, imageWidth, imageHeight);
 		PixelDouble curPixelDouble = null;
-
-		newImage.beginPixelUpdate();
+		minMax= getMinMax(image);
 		
+		newImage.beginPixelUpdate();
 		for (int x = 0; x < imageWidth; x++) {
 			for (int y = 0; y < imageHeight; y++) {
-				curPixelDouble = image.getPixel(x,y);
+				curPixelDouble = image.getPixel(x, y);
 				double ROUGE = curPixelDouble.getRed();
 				double VERT = curPixelDouble.getGreen();
 				double BLEU = curPixelDouble.getBlue();
 				double ALPHA = curPixelDouble.getAlpha();
-				
-				newImage.setPixel(x, y, new Pixel((int)(normalisation(ROUGE,ROUGE,VERT,BLEU)),
-												  (int)(normalisation(VERT,ROUGE,VERT,BLEU)),
-												  (int)(normalisation(BLEU,ROUGE,VERT,BLEU)),
-												  (int)(normalisation(ALPHA,ALPHA,ALPHA,ALPHA))));
+
+				newImage.setPixel(
+						x,
+						y,
+						new Pixel(
+								(int) (normalisation(ROUGE)),
+								(int) (normalisation(VERT)),
+								(int) (normalisation(BLEU)),
+								(int) (ALPHA)));
 			}
 		}
-		
-		
+
 		newImage.endPixelUpdate();
 		return newImage;
-		
-	 }
-	double normalisation(double cone, double R,double V, double B){
-		double calcul;
-		calcul = Math.abs(cone);
-		return ((calcul/(R+V+B))*255);
+
 	}
-	
+
+	public double[] getMinMax(ImageDouble image) {
+		int imageWidth = image.getImageWidth();
+		int imageHeight = image.getImageHeight();
+		PixelDouble curPixelDouble = null;
+		double MIN = 255;
+
+		double MAX = 0;
+
+		for (int x = 0; x < imageWidth; x++) {
+			for (int y = 0; y < imageHeight; y++) {
+				curPixelDouble = image.getPixel(x, y);
+
+				// CALCUL DU MIN
+				if (MIN > curPixelDouble.getRed()) {
+					MIN = curPixelDouble.getRed();
+				}
+				if (MIN > curPixelDouble.getGreen()) {
+					MIN = curPixelDouble.getGreen();
+				}
+				if (MIN > curPixelDouble.getBlue()) {
+					MIN = curPixelDouble.getBlue();
+				}
+				// CALCUL DU MAX
+				if (MAX < curPixelDouble.getRed()) {
+					MAX = curPixelDouble.getRed();
+				}
+				if (MAX < curPixelDouble.getGreen()) {
+					MAX = curPixelDouble.getGreen();
+				}
+				if (MAX < curPixelDouble.getBlue()) {
+					MAX = curPixelDouble.getBlue();
+				}
+			}
+		}
+		double[] tab = new double[2];
+		tab[0] = MIN;
+		tab[1] = MAX;
+		return tab;
+
+	}
+
+	double normalisation(double cone) {
+		double calcul;
+		cone= ((cone-minMax[0])*minMax[1])/255;
+		calcul = Math.abs(cone);
+		return calcul;
+	}
+
 }
