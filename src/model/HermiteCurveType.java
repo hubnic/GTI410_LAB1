e55e3e0cf0f9412767df0d2565db11009c2e8723
Nicolas Hubert <nicolas.hubert.1@ens.etsmtl.ca>
2015-07-09 16:19:51 -0400

    hermite corrigé

--- src/model/HermiteCurveType.java
+++ src/model/HermiteCurveType.java
@@ -65,24 +65,25 @@ public class HermiteCurveType extends CurveType {
 	 * @see model.CurveType#evalCurveAt(java.util.List, double)
 	 */
 	public Point evalCurveAt(List controlPoints, double t) {
-		ControlPoint r1 = new ControlPoint((((ControlPoint)controlPoints.get(2)).getCenter().getX() - ((ControlPoint)controlPoints.get(0)).getCenter().getX()),
-				(((ControlPoint)controlPoints.get(2)).getCenter().getY() - ((ControlPoint)controlPoints.get(0)).getCenter().getY()));
-		ControlPoint r2 = new ControlPoint((((ControlPoint)controlPoints.get(3)).getCenter().getX() - ((ControlPoint)controlPoints.get(1)).getCenter().getX()),
-				(((ControlPoint)controlPoints.get(3)).getCenter().getY() - ((ControlPoint)controlPoints.get(1)).getCenter().getY()));
+		ControlPoint r1 = new ControlPoint((((ControlPoint)controlPoints.get(1)).getCenter().getX() - ((ControlPoint)controlPoints.get(0)).getCenter().getX()),
+				(((ControlPoint)controlPoints.get(1)).getCenter().getY() - ((ControlPoint)controlPoints.get(0)).getCenter().getY()));
+		ControlPoint r2 = new ControlPoint((((ControlPoint)controlPoints.get(3)).getCenter().getX() - ((ControlPoint)controlPoints.get(2)).getCenter().getX()),
+				(((ControlPoint)controlPoints.get(3)).getCenter().getY() - ((ControlPoint)controlPoints.get(2)).getCenter().getY()));
 
 
 		System.out.println("Point 1  (" + ((ControlPoint)controlPoints.get(0)).getCenter().getX()+","+((ControlPoint)controlPoints.get(0)).getCenter().getY()+")");
 		System.out.println("Controle 1  (" + ((ControlPoint)controlPoints.get(2)).getCenter().getX()+","+((ControlPoint)controlPoints.get(2)).getCenter().getY()+")");
 		System.out.println("Point R1  (" + r1.getCenter().getX()+","+r1.getCenter().getY()+")");
-		System.out.println("Point 4  (" + ((ControlPoint)controlPoints.get(1)).getCenter().getX()+","+((ControlPoint)controlPoints.get(1)).getCenter().getY()+")");
+		System.out.println("Point 4  (" + ((ControlPoint)controlPoints.get(3)).getCenter().getX()+","+((ControlPoint)controlPoints.get(3)).getCenter().getY()+")");
 		System.out.println("Controle 4  (" + ((ControlPoint)controlPoints.get(3)).getCenter().getX()+","+((ControlPoint)controlPoints.get(3)).getCenter().getY()+")");
 		System.out.println("Point R2  (" + r2.getCenter().getX()+","+r2.getCenter().getY()+")");
 
 		List tVector = Matrix.buildRowVector4(t*t*t, t*t, t, 1);
 		List gVector = Matrix.buildColumnVector4(((ControlPoint) controlPoints.get(0)).getCenter(),
-				((ControlPoint) controlPoints.get(1)).getCenter(),
+				((ControlPoint) controlPoints.get(3)).getCenter(),
 				(r1.getCenter()),
-				(r2.getCenter()));
+				(r2.getCenter())
+				);
 		Point p = Matrix.eval(tVector, matrix, gVector);
 		return p;
 	}
